5ca8282d0f995eac361b47371b121c44a446aa81
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-07-15 11:58:08 -0400

    Add Netlify ID

--- config.js
+++ config.js
@@ -1,63 +1,63 @@
-{
-  "property": {
-    "streetAddress": "403 Oakhill Drive",
-    "city": "Brantford",
-    "cityStateZip": "Brantford, ON N3T 0L2",
-    "heroSubtitle": "Country feel close to the city with high-end finishes, a detached garage with hydro, and potential in-law suite. Quiet, mature neighbourhood, move-in ready. Listing Price: $799,000. Beds: 3+1, Baths: 2. Open House: June 21, 2025. Register to receive the complete Home & Neighbourhood Info Package, virtual tour, floor plans, and comparables.",
-    "backgroundImageUrl": "https://api.typeform.com/responses/files/f7da697bc5e01b20252bb22dac56f1d7f389cb8686a2e1f81fc5b325276a673d/kitchen.jpg"
+const config = {
+  property: {
+    streetAddress: "403 Oakhill Drive",
+    city: "Brantford",
+    cityStateZip: "Brantford, ON N3T 0L2",
+    heroSubtitle: "Country feel close to the city with high-end finishes, a detached garage with hydro, and potential in-law suite. Quiet, mature neighbourhood, move-in ready. Listing Price: $799,000. Beds: 3+1, Baths: 2. Open House: June 21, 2025. Register to receive the complete Home & Neighbourhood Info Package, virtual tour, floor plans, and comparables.",
+    backgroundImageUrl: "https://api.typeform.com/responses/files/f7da697bc5e01b20252bb22dac56f1d7f389cb8686a2e1f81fc5b325276a673d/kitchen.jpg"
   },
-  "realtor": {
-    "fullName": "Mel Ross",
-    "title": "REALTOR®",
-    "phone": null,
-    "email": "mel@theriserealtygroup.ca",
-    "bio": "Rise Realty Group was established with one core principle in mind: to provide the best real estate experience possible. We bring over 20 years of experience and a deep commitment to our community. Our mission goes beyond simply facilitating the buying and selling of homes; it’s about building relationships and fostering trust. At Rise Realty Group, we are here to raise your expectations and deliver exceptional results.",
-    "photoUrl": "https://prod-files-secure.s3.us-west-2.amazonaws.com/8e0c10a0-da43-409c-b191-91135b7161ff/22c01a4b-310d-4097-a0f7-5e664f5b7ed5/028A4024.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIAZI2LB466Z4GGPTY4%2F20250715%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20250715T155606Z&X-Amz-Expires=3600&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEC8aCXVzLXdlc3QtMiJHMEUCIQDHyZcC3WEGKTXOr9Z7No6r8M%2FGscWOjE4l2Hu2myNAqgIgZHzzll%2B%2BPiVxdi6VRfBes%2FJWI%2Bq5%2FojL7p91GjXKwqwq%2FwMISBAAGgw2Mzc0MjMxODM4MDUiDHzi7uX3YqE0Rc7TiCrcAw0quD65lYeOiJI8teZ0IM%2FYd8HtJPNlXDrrIwIli3ymfArlYSY6IBAgKTQnp9OFbTbtaIcXZYWpow23KK%2BTD%2FoEZ2a%2BI%2FuiRQUwETvNdgSTcmc6z7tiZ3vo7vZQFElJNv%2BQ%2BpW28%2FsdQ26htrnRXnBoCV7pvy9xukxREXMZvQ2cmmyy9%2BNe%2BZxap4h3mR0%2FVuQd6ohOaVi4tqz9kM%2F%2BYKNGOroq7J6xjZrYhEILASchZqpu79ByqTbx%2BGJmaGFiO3ZxVf78sECE4cUBvJU9%2BSDRzijFdImlN36GIpDn9J1Y2QCQBrJqn9KsFdhoLCogZwcl73v3JEsL64BEoGR4YFw783DT26JnezB%2BehDRgixK3DgE4%2Bq3UHj3WIYVqW6ntltsLLRQ9T8UZs1k5KlIccu3E%2BjiiNoFo94yMym8od2z0Wy28ZiBqeldCbQst6mRV4EaDlnnX3h%2B8oj6vMHqZCcwQ5Cn1Vv%2FAfrXD%2BuDk6HRoNythrTS9RM1%2BgcSVDoYUgXQkBwHq%2BwFsgLwRq82NqaTnIvs6Gq1YTw3ap5dToL5MPSDPTcJwCbQFX9fJsPriaJneQrPv48rrrI%2FVxpYaGhgOAldSpUswSTRyj0lJFhGojeoFDrtm7zip5RbMOHZ2cMGOqUBu3juJ0M2p5MZTgvd9kaARXy1cHquDmiymrDBdqH3xt5ECBozAcgX1X0%2FucJATcQwjQ2SMELujPEHGrlPPt2b0zEK5%2BHXeQNJrjb0v9FsPon4iTVT2WRRN6dvi7oN0rBvH2hNuplnFs9pRC%2BbuDupgglVdJK9cIyrauRp597QBnATF7fiPRfLtIZ%2F%2BVGGf0bsYXTt95SpiqfrDtrSH7fs%2BGTw6Rsi&X-Amz-Signature=73e821f08541c3feea42364d48c4546caacbf3e62fde0525f53393aeb81efd24&X-Amz-SignedHeaders=host&x-amz-checksum-mode=ENABLED&x-id=GetObject",
-    "social": {
-      "facebook": "https://www.facebook.com/theriserealtygroup/",
-      "instagram": "ashleyveldkamp",
-      "website": "https://theriserealtygroup.ca/"
+  realtor: {
+    fullName: "Mel Ross",
+    title: "REALTOR®",
+    phone: null,
+    email: "mel@theriserealtygroup.ca",
+    bio: "Rise Realty Group was established with one core principle in mind: to provide the best real estate experience possible. We bring over 20 years of experience and a deep commitment to our community. Our mission goes beyond simply facilitating the buying and selling of homes; it’s about building relationships and fostering trust. At Rise Realty Group, we are here to raise your expectations and deliver exceptional results.",
+    photoUrl: "https://prod-files-secure.s3.us-west-2.amazonaws.com/8e0c10a0-da43-409c-b191-91135b7161ff/22c01a4b-310d-4097-a0f7-5e664f5b7ed5/028A4024.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIAZI2LB466Z4GGPTY4%2F20250715%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20250715T155606Z&X-Amz-Expires=3600&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEC8aCXVzLXdlc3QtMiJHMEUCIQDHyZcC3WEGKTXOr9Z7No6r8M%2FGscWOjE4l2Hu2myNAqgIgZHzzll%2B%2BPiVxdi6VRfBes%2FJWI%2Bq5%2FojL7p91GjXKwqwq%2FwMISBAAGgw2Mzc0MjMxODM4MDUiDHzi7uX3YqE0Rc7TiCrcAw0quD65lYeOiJI8teZ0IM%2FYd8HtJPNlXDrrIwIli3ymfArlYSY6IBAgKTQnp9OFbTbtaIcXZYWpow23KK%2BTD%2FoEZ2a%2BI%2FuiRQUwETvNdgSTcmc6z7tiZ3vo7vZQFElJNv%2BQ%2BpW28%2FsdQ26htrnRXnBoCV7pvy9xukxREXMZvQ2cmmyy9%2BNe%2BZxap4h3mR0%2FVuQd6ohOaVi4tqz9kM%2F%2BYKNGOroq7J6xjZrYhEILASchZqpu79ByqTbx%2BGJmaGFiO3ZxVf78sECE4cUBvJU9%2BSDRzijFdImlN36GIpDn9J1Y2QCQBrJqn9KsFdhoLCogZwcl73v3JEsL64BEoGR4YFw783DT26JnezB%2BehDRgixK3DgE4%2Bq3UHj3WIYVqW6ntltsLLRQ9T8UZs1k5KlIccu3E%2BjiiNoFo94yMym8od2z0Wy28ZiBqeldCbQst6mRV4EaDlnnX3h%2B8oj6vMHqZCcwQ5Cn1Vv%2FAfrXD%2BuDk6HRoNythrTS9RM1%2BgcSVDoYUgXQkBwHq%2BwFsgLwRq82NqaTnIvs6Gq1YTw3ap5dToL5MPSDPTcJwCbQFX9fJsPriaJneQrPv48rrrI%2FVxpYaGhgOAldSpUswSTRyj0lJFhGojeoFDrtm7zip5RbMOHZ2cMGOqUBu3juJ0M2p5MZTgvd9kaARXy1cHquDmiymrDBdqH3xt5ECBozAcgX1X0%2FucJATcQwjQ2SMELujPEHGrlPPt2b0zEK5%2BHXeQNJrjb0v9FsPon4iTVT2WRRN6dvi7oN0rBvH2hNuplnFs9pRC%2BbuDupgglVdJK9cIyrauRp597QBnATF7fiPRfLtIZ%2F%2BVGGf0bsYXTt95SpiqfrDtrSH7fs%2BGTw6Rsi&X-Amz-Signature=73e821f08541c3feea42364d48c4546caacbf3e62fde0525f53393aeb81efd24&X-Amz-SignedHeaders=host&x-amz-checksum-mode=ENABLED&x-id=GetObject",
+    social: {
+      facebook: "https://www.facebook.com/theriserealtygroup/",
+      instagram: "ashleyveldkamp",
+      website: "https://theriserealtygroup.ca/"
     }
   },
-  "brokerage": {
-    "name": "Real Broker LTD",
-    "logoUrl": "https://prod-files-secure.s3.us-west-2.amazonaws.com/8e0c10a0-da43-409c-b191-91135b7161ff/9e0cb426-7be8-42ce-9e46-be8012159b0b/RRG_Grey_CMYK.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIAZI2LB466Z4GGPTY4%2F20250715%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20250715T155606Z&X-Amz-Expires=3600&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEC8aCXVzLXdlc3QtMiJHMEUCIQDHyZcC3WEGKTXOr9Z7No6r8M%2FGscWOjE4l2Hu2myNAqgIgZHzzll%2B%2BPiVxdi6VRfBes%2FJWI%2Bq5%2FojL7p91GjXKwqwq%2FwMISBAAGgw2Mzc0MjMxODM4MDUiDHzi7uX3YqE0Rc7TiCrcAw0quD65lYeOiJI8teZ0IM%2FYd8HtJPNlXDrrIwIli3ymfArlYSY6IBAgKTQnp9OFbTbtaIcXZYWpow23KK%2BTD%2FoEZ2a%2BI%2FuiRQUwETvNdgSTcmc6z7tiZ3vo7vZQFElJNv%2BQ%2BpW28%2FsdQ26htrnRXnBoCV7pvy9xukxREXMZvQ2cmmyy9%2BNe%2BZxap4h3mR0%2FVuQd6ohOaVi4tqz9kM%2F%2BYKNGOroq7J6xjZrYhEILASchZqpu79ByqTbx%2BGJmaGFiO3ZxVf78sECE4cUBvJU9%2BSDRzijFdImlN36GIpDn9J1Y2QCQBrJqn9KsFdhoLCogZwcl73v3JEsL64BEoGR4YFw783DT26JnezB%2BehDRgixK3DgE4%2Bq3UHj3WIYVqW6ntltsLLRQ9T8UZs1k5KlIccu3E%2BjiiNoFo94yMym8od2z0Wy28ZiBqeldCbQst6mRV4EaDlnnX3h%2B8oj6vMHqZCcwQ5Cn1Vv%2FAfrXD%2BuDk6HRoNythrTS9RM1%2BgcSVDoYUgXQkBwHq%2BwFsgLwRq82NqaTnIvs6Gq1YTw3ap5dToL5MPSDPTcJwCbQFX9fJsPriaJneQrPv48rrrI%2FVxpYaGhgOAldSpUswSTRyj0lJFhGojeoFDrtm7zip5RbMOHZ2cMGOqUBu3juJ0M2p5MZTgvd9kaARXy1cHquDmiymrDBdqH3xt5ECBozAcgX1X0%2FucJATcQwjQ2SMELujPEHGrlPPt2b0zEK5%2BHXeQNJrjb0v9FsPon4iTVT2WRRN6dvi7oN0rBvH2hNuplnFs9pRC%2BbuDupgglVdJK9cIyrauRp597QBnATF7fiPRfLtIZ%2F%2BVGGf0bsYXTt95SpiqfrDtrSH7fs%2BGTw6Rsi&X-Amz-Signature=e1b091e45b5718ff874698f9946c4cee96906895e247b1a6866187f565805842&X-Amz-SignedHeaders=host&x-amz-checksum-mode=ENABLED&x-id=GetObject",
-    "address": "218 Brant Avenue, Brantford ON N3T 3H9"
+  brokerage: {
+    name: "Real Broker LTD",
+    logoUrl: "https://prod-files-secure.s3.us-west-2.amazonaws.com/8e0c10a0-da43-409c-b191-91135b7161ff/9e0cb426-7be8-42ce-9e46-be8012159b0b/RRG_Grey_CMYK.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIAZI2LB466Z4GGPTY4%2F20250715%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20250715T155606Z&X-Amz-Expires=3600&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEC8aCXVzLXdlc3QtMiJHMEUCIQDHyZcC3WEGKTXOr9Z7No6r8M%2FGscWOjE4l2Hu2myNAqgIgZHzzll%2B%2BPiVxdi6VRfBes%2FJWI%2Bq5%2FojL7p91GjXKwqwq%2FwMISBAAGgw2Mzc0MjMxODM4MDUiDHzi7uX3YqE0Rc7TiCrcAw0quD65lYeOiJI8teZ0IM%2FYd8HtJPNlXDrrIwIli3ymfArlYSY6IBAgKTQnp9OFbTbtaIcXZYWpow23KK%2BTD%2FoEZ2a%2BI%2FuiRQUwETvNdgSTcmc6z7tiZ3vo7vZQFElJNv%2BQ%2BpW28%2FsdQ26htrnRXnBoCV7pvy9xukxREXMZvQ2cmmyy9%2BNe%2BZxap4h3mR0%2FVuQd6ohOaVi4tqz9kM%2F%2BYKNGOroq7J6xjZrYhEILASchZqpu79ByqTbx%2BGJmaGFiO3ZxVf78sECE4cUBvJU9%2BSDRzijFdImlN36GIpDn9J1Y2QCQBrJqn9KsFdhoLCogZwcl73v3JEsL64BEoGR4YFw783DT26JnezB%2BehDRgixK3DgE4%2Bq3UHj3WIYVqW6ntltsLLRQ9T8UZs1k5KlIccu3E%2BjiiNoFo94yMym8od2z0Wy28ZiBqeldCbQst6mRV4EaDlnnX3h%2B8oj6vMHqZCcwQ5Cn1Vv%2FAfrXD%2BuDk6HRoNythrTS9RM1%2BgcSVDoYUgXQkBwHq%2BwFsgLwRq82NqaTnIvs6Gq1YTw3ap5dToL5MPSDPTcJwCbQFX9fJsPriaJneQrPv48rrrI%2FVxpYaGhgOAldSpUswSTRyj0lJFhGojeoFDrtm7zip5RbMOHZ2cMGOqUBu3juJ0M2p5MZTgvd9kaARXy1cHquDmiymrDBdqH3xt5ECBozAcgX1X0%2FucJATcQwjQ2SMELujPEHGrlPPt2b0zEK5%2BHXeQNJrjb0v9FsPon4iTVT2WRRN6dvi7oN0rBvH2hNuplnFs9pRC%2BbuDupgglVdJK9cIyrauRp597QBnATF7fiPRfLtIZ%2F%2BVGGf0bsYXTt95SpiqfrDtrSH7fs%2BGTw6Rsi&X-Amz-Signature=e1b091e45b5718ff874698f9946c4cee96906895e247b1a6866187f565805842&X-Amz-SignedHeaders=host&x-amz-checksum-mode=ENABLED&x-id=GetObject",
+    address: "218 Brant Avenue, Brantford ON N3T 3H9"
   },
-  "openHouse": {
-    "eventDate": "2025-06-21",
-    "bundleItems": [
+  openHouse: {
+    eventDate: "2025-06-21",
+    bundleItems: [
       {
-        "icon": "fas fa-link",
-        "text": "Property Listing",
-        "url": "https://track.pstmrk.it/3s/sites.ground2airmedia.com%2Fsites%2F403-oakhill-dr-brantford-on-n3t-0l2-16677199%2Fbranded/cUpU/nda9AQ/AQ/fb8ae521-c973-4fce-949e-4b226e146ddb/2/CDNrd2MM24"
+        icon: "fas fa-link",
+        text: "Property Listing",
+        url: "https://track.pstmrk.it/3s/sites.ground2airmedia.com%2Fsites%2F403-oakhill-dr-brantford-on-n3t-0l2-16677199%2Fbranded/cUpU/nda9AQ/AQ/fb8ae521-c973-4fce-949e-4b226e146ddb/2/CDNrd2MM24"
       },
       {
-        "icon": "fas fa-image",
-        "text": "Photos",
-        "url": "https://api.typeform.com/responses/files/deb5b6a9a2bf6b50f4b5b84c528a3de462899ace3773fba1871683a787dae9ac/front_of_house.jpg"
+        icon: "fas fa-image",
+        text: "Photos",
+        url: "https://api.typeform.com/responses/files/deb5b6a9a2bf6b50f4b5b84c528a3de462899ace3773fba1871683a787dae9ac/front_of_house.jpg"
       },
       {
-        "icon": "fas fa-video",
-        "text": "Branded Media Video",
-        "url": "https://youtube.com/shorts/HSqZdZ4-1NE?feature=share"
+        icon: "fas fa-video",
+        text: "Branded Media Video",
+        url: "https://youtube.com/shorts/HSqZdZ4-1NE?feature=share"
       },
       {
-        "icon": "fas fa-chart-bar",
-        "text": "Recent Neighborhood Sales (Comparables)",
-        "url": "https://drive.google.com/drive/folders/1QzqGNaXFfRg9sj0G0-KmG5X1Rl2GY5vd?usp=drive_link"
+        icon: "fas fa-chart-bar",
+        text: "Recent Neighborhood Sales (Comparables)",
+        url: "https://drive.google.com/drive/folders/1QzqGNaXFfRg9sj0G0-KmG5X1Rl2GY5vd?usp=drive_link"
       }
     ]
   },
-  "meta": {
-    "pageTitlePrefix": "🏡 Mel Ross Open House",
-    "navBrandLogoText": "OPEN HOUSE",
-    "privacyPolicyLink": "https://theriserealtygroup.ca/"
+  meta: {
+    pageTitlePrefix: "🏡 Mel Ross Open House",
+    navBrandLogoText: "OPEN HOUSE",
+    privacyPolicyLink: "https://theriserealtygroup.ca/"
   },
-  "settings": {
-    "adminWebhookUrl": "https://n8n.salesgenius.co/webhook/openhouseupdate",
-    "visitorWebhookUrl": "https://n8n.salesgenius.co/webhook/openhouse",
-    "confettiColors": [
+  settings: {
+    adminWebhookUrl: "https://n8n.salesgenius.co/webhook/openhouseupdate",
+    visitorWebhookUrl: "https://n8n.salesgenius.co/webhook/openhouse",
+    confettiColors: [
       "#72987D",
       "#F5EBDD",
       "#F8C185",
@@ -65,49 +65,49 @@
       "#E8E3E2"
     ]
   },
-  "modalQuestions": [
+  modalQuestions: [
     {
-      "id": "agent_status",
-      "questionText": "Are you currently exploring homes with a real estate agent?",
-      "options": [
-        { "value": "committed", "text": "Yes, I'm committed to an agent." },
-        { "value": "not_committed", "text": "No, I'm not committed to an agent." },
-        { "value": "gathering_info", "text": "Just gathering information at this stage." }
+      id: "agent_status",
+      questionText: "Are you currently exploring homes with a real estate agent?",
+      options: [
+        { value: "committed", text: "Yes, I'm committed to an agent." },
+        { value: "not_committed", text: "No, I'm not committed to an agent." },
+        { value: "gathering_info", text: "Just gathering information at this stage." }
       ]
     },
     {
-      "id": "interest_level",
-      "questionText": "How interested are you in properties like this?",
-      "options": [
-        { "value": "very_interested", "text": "Very interested, actively looking." },
-        { "value": "somewhat_interested", "text": "Somewhat interested, keeping options open." },
-        { "value": "just_browsing", "text": "Just browsing for now." }
+      id: "interest_level",
+      questionText: "How interested are you in properties like this?",
+      options: [
+        { value: "very_interested", text: "Very interested, actively looking." },
+        { value: "somewhat_interested", text: "Somewhat interested, keeping options open." },
+        { value: "just_browsing", text: "Just browsing for now." }
       ]
     },
     {
-      "id": "neighborhood_engagement",
-      "questionText": "How do you usually enjoy the neighborhood?",
-      "options": [
-        { "value": "resident_explorer", "text": "I live here and love exploring locally." },
-        { "value": "considering_move", "text": "I'm considering moving to this area." },
-        { "value": "just_visiting", "text": "Just visiting for the open house." }
+      id: "neighborhood_engagement",
+      questionText: "How do you usually enjoy the neighborhood?",
+      options: [
+        { value: "resident_explorer", text: "I live here and love exploring locally." },
+        { value: "considering_move", text: "I'm considering moving to this area." },
+        { value: "just_visiting", text: "Just visiting for the open house." }
       ]
     },
     {
-      "id": "feature_attraction",
-      "questionText": "What feature attracted you to this home?",
-      "options": [
-        { "value": "design_layout", "text": "The design and layout." },
-        { "value": "location_neighborhood", "text": "The location and neighborhood." },
-        { "value": "home_price", "text": "The price of the home." }
+      id: "feature_attraction",
+      questionText: "What feature attracted you to this home?",
+      options: [
+        { value: "design_layout", text: "The design and layout." },
+        { value: "location_neighborhood", text: "The location and neighborhood." },
+        { value: "home_price", text: "The price of the home." }
       ]
     }
   ],
-  "deploymentInfo": {
-    "repoName": "Open-House-508",
-    "repoUrl": "https://github.com/arslvn93/Open-House-508",
-    "tag": "Open House 403 Oakhill Drive",
-    "netlifyUrl": "",
-    "siteId": "1020273780"
+  deploymentInfo: {
+    repoName: "Open-House-508",
+    repoUrl: "https://github.com/arslvn93/Open-House-508",
+    tag: "Open House 403 Oakhill Drive",
+    netlifyUrl: "",
+    siteId: "6d4ca4b9-2734-43a6-bc8b-b7de95678168"
   }
 }
